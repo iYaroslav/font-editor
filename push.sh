05104e1813613d3e10094b75e9dba297acd95957
iYaroslav <samardak.yaroslav@gmail.com>
2020-12-01 14:21:00 +0500

    Update push script

--- push.sh
+++ push.sh
@@ -4,6 +4,8 @@ if [[ -z "$(git status --untracked-files=no --porcelain)" ]]; then
   yarn version --patch
   git push
   git push --tags
+
+  open $(git remote -v | grep fetch | awk '{print $2}' | sed 's/git@/http:\/\//' | sed 's/com:/com\//')| head -n1
 else
   echo 'Commit changes before deploy!'
 fi
